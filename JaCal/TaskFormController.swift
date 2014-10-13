57d2e42c3414ce452e56dedfcf96679c390f46a1
applede <jksong3@gmail.com>
2014-10-13 22:14:04 +0900

    issue #7 해결

--- JaCal/TaskFormController.swift
+++ JaCal/TaskFormController.swift
@@ -37,27 +37,20 @@ class TaskFormController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
     
-    // Do any additional setup after loading the view.
     self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .Done, target: self, action: "done:")
     if let t = task {
       icon.setTitle(t.icon, forState: .Normal)
       desc.text = t.title
     }
+    let tap = UITapGestureRecognizer(target: self.view, action: "endEditing:")
+    tap.cancelsTouchesInView = false
+    view.addGestureRecognizer(tap)
   }
-  
+
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
   }
-  
-  
-  // MARK: - Navigation
-  
-  // In a storyboard-based application, you will often want to do a little preparation before navigation
-//  override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject!) {
-//    // Get the new view controller using segue.destinationViewController.
-//    // Pass the selected object to the new view controller.
-//  }
 
   func collectionView(collectionView: UICollectionView!, didSelectItemAtIndexPath indexPath: NSIndexPath!) {
   }
